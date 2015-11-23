d51eb4ebdf5831bbf0bf0e70b79ccb706b4fb5d8
Pavel Maslov <pavel.maslov@cloud.si>
2015-11-23 10:14:41 +0000

    ssl script tweaks

--- ssl.sh
+++ ssl.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-keytool -genkey -noprompt \
-  -alias localhost \
-  -dname "CN=localhost, OU=LIIS, O=FRI, L=Pavel, S=Maslov, C=SI" \
-  -keyalg RSA \
-  -keystore keycloak.jks \
-  -storepass secret \
-  -keypass secret \
-  -validity 10950
+rm -f keycloak.jks
+
+function genkey {
+  keytool -genkey -noprompt \
+    -alias keycloak-$1 \
+    -dname "CN=$1, OU=LIIS, O=FRI, L=Pavel, S=Maslov, C=SI" \
+    -keyalg RSA \
+    -keystore keycloak.jks \
+    -storepass secret \
+    -keypass secret \
+    -validity 10950
+}
+
+genkey auth.maslick.com
